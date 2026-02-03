98b76319d59d00c3e4b2ba1f92d3edc5aadfeabf
Kentar01337 <dietz.billy-joe@mailbox.org>
2026-02-03 09:15:01 +0100

    gitignore gefixt

---
+++ 09-IntersectionTypes/script.js
@@ -0,0 +1,18 @@
+"use strict";
+/**
+ * * Intersection Types ähnlich Extended Interfaces, aber eher
+ * * Kombination von TypeAliases (keine echte Erweiterung)
+ */
+const user1 = {
+    name: 'Andreas',
+    age: 25,
+    courses: ['C', 'C++', 'Python', 'TypeScript']
+};
+console.log(user1);
+const user2 = {
+    name: 'Sabine',
+    age: 42,
+    courses: ['Java', 'C#'],
+    sex: 'female'
+};
+console.log(user2);
